2b8465df43ac12a97806509e523a5406845827e0
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-05-28 22:18:22 +0530

    Add files via upload

--- Chauhan.py
+++ Chauhan.py
@@ -1,5 +1,7 @@
+""" Create a file a.txt and enter the values from 50 to 500 with space"""
+
 f=open("a.txt",'w')
 for i in range(50,501):
     x=str(i)
-    f.write(x)
+    f.write(x+' ')
 f.close()
